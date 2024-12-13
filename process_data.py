45b055038d88f617e890b51245b3e958bb7172b8
Object278 <70797991+Object278@users.noreply.github.com>
2024-12-13 17:33:34 +0000

    framework 5.6.2 Agent

--- process_data.py
+++ process_data.py
@@ -10,10 +10,15 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import functools
 
+from langchain.prompts import PromptTemplate
+from langchain.prompts import PartialPromptTemplate
+
 '''
 环境变量：
 webdriver安装地址
 本地vllm部署的大模型的端口
+prompt最长token数，如果过长可能会需要使用rag获取以前的prompt里面的关键部分
+模型名称
 '''
 
 def action_error_detector(func):
@@ -39,7 +44,6 @@ def action_error_detector(func):
             return False, error_message
     return wrapper
 
-
 class RoundState():
     def __init__(self):
         self.action = None
@@ -52,11 +56,76 @@ class RoundState():
     def set_action(self, action: str):
         self.action = action
 
-    def append_observation(self, observation: list[str]):
+    def append_observation(self, observation: str):
         self.observation.append(observation)
 
+class RoundPrompt():
+    def __init__(self):
+        self.round = ""
+        self.observation = ""
+        self.action = ""
+
+    def __str__(self):
+        return f"{self.round}{self.observation}{self.action}"
+
 class RoundStateList():
-    pass
+    def __init__(self):
+        # 预先初始化round 0，每次添加action之后，才会初始化下一个round
+        # 一个round中，observation列表可以有多个或者没有，但是action必须有一个
+        self.state_list = [RoundState()]
+        self.prompt_list = [RoundPrompt()]
+        self.latest_prompt_index = 0 # 这是roundstate之中当前最新的，也是唯一一个没有填充完成的prompt所在的index
+        self.completed_prompt = '' # 这是所有已经填充完毕的prompt所组成的字符串
+        self.action_prompt = PromptTemplate(
+            input_variables=["action"],
+            template="assistant:\n{action}\n"    
+        )
+        self.observation_prompt = PromptTemplate(
+            input_variables=["observation"],
+            template="user:\n{observation}\n"
+        )
+        self.round_prompt = PromptTemplate(
+            input_variables=["index"],
+            template="Round {index}\n"
+        )
+        
+    # TODO 处理index错误时候怎么办，使用的时候必须遵守流程图定义的顺序
+    def add_action(self, index: int, action: str):
+        if index != self.latest_prompt_index:
+            print("error")
+        self.state_list[index].set_action(action)
+        self.prompt_list[index].action = self.action_prompt.format(action=action)
+        self.state_list.append(RoundState())
+        self.prompt_list.append(RoundPrompt())
+        self.completed_prompt = self.completed_prompt + str(self.prompt_list[index])
+        self.latest_prompt_index += 1
+
+    def add_observation(self, index: int, observation: str):
+        if index != self.latest_prompt_index:
+            print("error")
+        self.state_list[index].append_observation(observation)
+        self.prompt_list[index].observation = self.observation_prompt.format(observation=observation)
+        self.prompt_list[index].round = self.round_prompt.format(index=index)
+
+    def get_new_prompt(self):
+        return self.completed_prompt + str(self.prompt_list[self.latest_prompt_index])
+    
+    def get_state(self, index: int):
+        if index > 0 and index < len(self.state_list):
+            return self.state_list[index]
+        return None
+    
+    def update_state(self, index: int, round_state: RoundState):
+        if index > 0 and index < len(self.state_list):
+            self.state_list[index] = round_state
+            self.completed_prompt = str(self)
+
+    def __str__(self):
+        ret = ""
+        # 只输出已经完成的round
+        for i in range(0, self.latest_prompt_index):
+            ret = ret + str(self.prompt_list[i])
+        return ret
 
 # 对应一个selenium webdriver对象，可以独立操作网页并且进行推理。
 # 对于web应用，一个websocket session内部一个Agent和多个用户。用户请求通过前端传递给Agent，Agent的操作大家都能看到
@@ -72,16 +141,23 @@ class Agent():
         self.action_chains = ActionChains(self.driver)
         self.state = RoundStateList()
 
+        self.round = 0
+        self.user_instruction = None
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         print("Exiting with: " + str(exc_type) + str(exc_val), str(exc_tb))
         self.driver.quit()
 
     '''
-    汇报
+    思考
     '''    
-    def add_state():
-        # action是模型返回的，user字段是自己构建的，在同一个round，二者不同时出现，需要分别加入
-        pass
+    def ask_oracle(self):
+        prompt = self.generate_prompt()
+        # 提问
+
+        # 解析回答
+        action = ""
+        self.add_action(action)
 
     '''
     观察
@@ -98,12 +174,15 @@ class Agent():
         else:
             print("未检测到URL，访问本地主机的FastAPI服务。")
             html_raw = fetch_html_with_selenium("http://localhost:8000")
-        return self.clean_html_and_add_ids(html_raw)
+        html_clean = self.clean_html_and_add_ids(html_raw)
+        self.add_observation(html_clean)
 
     '''
     行动
     '''
     def do(self, action, **kwargs):
+            self.round += 1 
+            # TODO汇报任务成功与否
             if action == "Click":
                 self.click_element(kwargs["element"])
             elif action == "Hover":
@@ -134,18 +213,30 @@ class Agent():
                 self.exit_browser()
 
 
-
-    def run():
+    # 一次用户请求执行完了，准备下一次
+    def reset():
         pass
 
     '''
-    汇报部分（根据观察和行动结果更新state，生成prompt）
+    内部状态管理部分（根据观察和行动结果更新state，生成prompt）
     '''
+    def set_user_instruction(self, instruction: str):
+        # action是模型返回的，user字段是自己构建的，在同一个round，二者不同时出现，需要分别加入
+        self.user_instruction = instruction
 
+    # observation 和 action 都只能添加在当前round的state中，根据规划的工作流程，每次执行action（do函数），round加1，不论action成功还是失败
+    def add_observation(self, observation: str):
+        self.state.add_observation(self.round, observation)
+
+    def add_action(self, action: str):
+        self.state.add_action(self.round, action)
+
+    def generate_prompt(self):
+        # 根据上下文限制处理prompt长度，以及其他的参数
+        return f"Task Instruction: {self.user_instruction}\n{self.state.get_new_prompt()}"
     '''
     观察部分辅助函数
     '''
-
     def extract_url(self, query):
         """
         从查询字符串中提取URL。
@@ -195,7 +286,7 @@ class Agent():
 
         for tag in soup.find_all(True):  # True 匹配所有标签，也许还可以创建一个索引，保存哪个action_id对应哪个组件，方便后期action文件操作
             if tag.name in interactive_tags:
-                # 为可交互的标签添加唯一的 id
+                # 为可交互的标签添加唯一的 id TODO 再添加每个可交互元素的data-bbox
                 if not tag.has_attr('action_id'):
                     tag['action_id'] = f'{id_counter}'
                     id_counter += 1
@@ -209,7 +300,6 @@ class Agent():
     '''
     行动部分辅助函数
     '''
-
     @action_error_detector
     def click_element(self, element_id):
         element = self.driver.find_element(By.ID, element_id)
